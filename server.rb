1e0466e5fb2290fcef1f91f2321c035984300793
LuisAlvarez99 <luis.alvarez0129@outlook.com>
2019-12-11 22:20:01 -0500

    moving back to master

--- server.rb
+++ server.rb
@@ -8,8 +8,9 @@ get '/' do
 end
 
 post '/' do
-    @p = params['food']
-    req = HTTParty.get(@@path, query: {app_id:ENV['EDAMAM_API_ID'],app_key:ENV['EDAMAM_API_KEY'],cuisineType:@p})
+  "we got the post request :)"
+    # @p = params['food']
+    # req = HTTParty.get(@@path, query: {app_id:ENV['EDAMAM_API_ID'],app_key:ENV['EDAMAM_API_KEY'],cuisineType:@p})
     # @res = JSON.parse(req.body)
-    puts req 
+    # puts req 
 end
