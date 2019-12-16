3dcf13b6e377979db2afa5b64452c226d02e1e96
LuisAlvarez99 <luis.alvarez0129@outlook.com>
2019-12-16 14:51:29 -0500

    added new pages and routes still not in use

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org/" do
-    gem 'httparty', '~> 0.17.1'
     gem 'curb', '~> 0.9.10'
     gem 'sinatra', '~> 2.0', '>= 2.0.7'
+    gem 'json', '~> 2.3'
 end
--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'curb'
 require 'httparty'
+require 'json'
 
 @@path = 'https://api.edamam.com/search'
 
@@ -11,13 +12,28 @@ end
 post '/' do
     @food = params['food']
     @num = params['to'].to_i
-    @arr = []
     req = Curl::Easy.perform(@@path + "?q=#{@food}&app_id=a2d96ba1&app_key=3e4694ce3477d438bf0a3c273eafd9e3&from=0&to=#{@num}&calories=591-722&health=alcohol-free")
     res = JSON.parse(req.body)
+    @arr = []
     x = 0
     while x < @num
-    @data = res["hits"][x]["recipe"]
-    @arr << @data
-    x += 1
+        @data = res["hits"][x]["recipe"]
+        @arr << @data
+        x += 1
     end
+    puts @data  
+    erb :food
+end
+
+
+get '/cake' do
+    erb :cake
+end
+
+get '/muffins' do
+    erb :muffins
+end
+
+get '/cookies' do
+    erb :cookies
 end
